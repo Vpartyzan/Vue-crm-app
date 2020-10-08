46778fc89eaaa07a30fd4e292c13531634f0b8a6
vPartyzan <peregrim.mt@gmail.com>
2020-10-08 16:17:33 +0200

    protected routes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -23,6 +23,7 @@
 <script>
 import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
+import messages from '@/utils/messages'
 
 export default {
   name: 'MainLayout',
@@ -39,6 +40,16 @@ export default {
   components: {
     Navbar,
     Sidebar
+  },
+  computed: {
+    error () {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error (fbError) {
+      this.$error(messages[fbError.code] || 'Что-то не так')
+    }
   }
 }
 
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
   logout: 'Вы вышли из системы',
+  login: 'Войдите в систему',
   'auth/user-not-found': 'Такого пользователя не существует',
   'auth/wrong-password': 'Неверный пароль',
   'auth/email-already-in-use': 'Данный e-mail уже используется'
